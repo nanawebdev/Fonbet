7ac8f1663eb474f479694317007fc454b8f2a713
nanawebdev <nanaprettysinger@gmail.com>
2020-12-10 15:56:53 +0300

    draft

--- top.js
+++ top.js
@@ -64,6 +64,7 @@ const getSportButtons = function (array) {
 getSportButtons(sportTitles)
 
 
+// Функция для генерирования рандомных данных в таблицу - для тестов
 
 // function generateRandomString(stringLength) {
 //     const letters = ['й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', ' ', 'ъ', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю']
